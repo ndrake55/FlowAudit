5cadf0f45dfd878fe45a6ec75c2ded70881af1fe
Neal Drake <ndrake55@gmail.com>
2026-02-08 13:47:20 -0800

    Revert middleware to Edge Runtime to fix 500 error

--- middleware.ts
+++ middleware.ts
@@ -17,7 +17,6 @@ export default clerkMiddleware(async (auth, req) => {
 });
 
 export const config = {
-    runtime: 'nodejs',
     matcher: [
         // Skip Next.js internals and all static files, unless found in search params
         '/((?!_next|api|trpc|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
