8837f6f3fc161e9960c80405d861a3db7165daad
Neal Drake <ndrake55@gmail.com>
2026-02-08 14:11:07 -0800

    Test runtime

--- middleware.ts
+++ middleware.ts
@@ -12,11 +12,13 @@ const isPublicRoute = createRouteMatcher([
 
 export default clerkMiddleware(async (auth, req) => {
     if (!isPublicRoute(req)) {
-        await auth.protect();
+        // console.log("Middleware running on:", req.url);
+        // await auth.protect();
     }
 });
 
 export const config = {
+    runtime: 'nodejs',
     matcher: [
         // Skip Next.js internals and all static files, unless found in search params
         '/((?!_next|api|trpc|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
